5bbc8190a22114fe8ceaece5dd53bac733ec9929
Joshua Byrd <phocks@gmail.com>
2020-03-13 07:48:56 +1000

    Worked out secrets etc

--- secret.sample.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  user: "username",
-  password: "password",
-  remoteRoot: "/remote",
-  host: "host.ftp.com",
-}
--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ const fs = require("fs");
 const FtpDeploy = require("ftp-deploy");
 const ftpDeploy = new FtpDeploy();
 
-const credentials = require("../secret");
+const credentials = require("./secret.json");
 
 const ORIGINAL_DATA_URL =
   "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 
 exports.main = async (req, res) => {
-  console.log(req.query);
-
   const [err, response] = await to(
     axios({
       method: "get",
@@ -35,7 +33,7 @@ exports.main = async (req, res) => {
 
   // Write json to tmp directory
   // Make it synchronous otherwise function will end prematurely
-  fs.writeFileSync("./tmp/data.json", JSON.stringify(parsed.data));
+  fs.writeFileSync("/tmp/data.json", JSON.stringify(parsed.data));
 
   // Upload to FTP
   if (req.query.ftp === "true") {
