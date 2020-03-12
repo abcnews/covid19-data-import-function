b65594d9e054114ff56230542752b32955f81df8
Joshua Byrd <phocks@gmail.com>
2020-03-12 16:13:45 +1000

    Make FTP optional

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const ORIGINAL_DATA_URL =
   "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 
 exports.main = async (req, res) => {
+  console.log(req.query);
+
   const [err, response] = await to(
     axios({
       method: "get",
@@ -36,28 +38,30 @@ exports.main = async (req, res) => {
   fs.writeFileSync("./tmp/data.json", JSON.stringify(parsed.data));
 
   // Upload to FTP
-  const [ftpErr, ftpResponse] = await to(
-    ftpDeploy.deploy({
-      user: credentials.user,
-      password: credentials.password,
-      host: credentials.host,
-      port: 21,
-      localRoot: "tmp",
-      remoteRoot: credentials.remoteRoot,
-      include: ["data.json"],
-      exclude: [],
-      deleteRemote: false,
-      forcePasv: true
-    })
-  );
+  if (req.query.ftp === "true") {
+    const [ftpErr, ftpResponse] = await to(
+      ftpDeploy.deploy({
+        user: credentials.user,
+        password: credentials.password,
+        host: credentials.host,
+        port: 21,
+        localRoot: "tmp",
+        remoteRoot: credentials.remoteRoot,
+        include: ["data.json"],
+        exclude: [],
+        deleteRemote: false,
+        forcePasv: true
+      })
+    );
 
-  if (ftpErr) {
-    console.log(ftpErr);
-    res.json("FTP error...");
-    return;
+    if (ftpErr) {
+      console.log(ftpErr);
+      res.json("FTP error...");
+      return;
+    }
+
+    console.log(ftpResponse);
   }
-
-  console.log(ftpResponse);
 
   // Return the data (just in case)
   res.json(parsed.data);
