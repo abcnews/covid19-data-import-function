e72f68d7bd5396f0a0b65df9133f1852a01a62db
Joshua Byrd <phocks@gmail.com>
2020-03-12 15:57:34 +1000

    Secret file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const axios = require("axios");
 const to = require("await-to-js").default;
 const Papa = require("papaparse");
+const fs = require("fs");
+const FtpDeploy = require("ftp-deploy");
+const ftpDeploy = new FtpDeploy();
+
+const credentials = require("../secret");
 
 const ORIGINAL_DATA_URL =
   "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 
-exports.dataImport = async (req, res) => {
+exports.main = async (req, res) => {
   const [err, response] = await to(
     axios({
       method: "get",
@@ -13,17 +18,52 @@ exports.dataImport = async (req, res) => {
     })
   );
 
+  // Catch fetch errors TOTO: handle errors properly
   if (err) {
     console.log(err);
     res.json("Error...");
     return;
-  } else {
-    const parsed = Papa.parse(response.data, {
-      header: true,
-      dynamicTyping: true
-    });
-    console.log(parsed);
-    res.json("OK!");
+  }
+
+  // Parse the CSV data
+  const parsed = Papa.parse(response.data, {
+    header: true,
+    dynamicTyping: true
+  });
+
+  console.log(parsed.data);
+
+  // Write json to tmp directory
+  // Make it synchronous otherwise function will end prematurely
+  fs.writeFileSync("./tmp/data.json", JSON.stringify(parsed.data));
+
+  // Upload to FTP
+  const [ftpErr, ftpResponse] = await to(
+    ftpDeploy.deploy({
+      user: credentials.user,
+      password: credentials.password,
+      host: credentials.host,
+      port: 21,
+      localRoot: "tmp",
+      remoteRoot: credentials.remoteRoot,
+      // include: ["*", "**/*"],      // this would upload everything except dot files
+      include: ["*"],
+      // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
+      exclude: [],
+      // delete ALL existing files at destination before uploading, if true
+      deleteRemote: false,
+      // Passive mode is forced (EPSV command is not sent)
+      forcePasv: true
+    })
+  );
+
+  if (ftpErr) {
+    console.log(ftpErr);
+    res.json("FTP error...");
     return;
   }
+
+  console.log(ftpResponse);
+
+  res.json("OK!");
 };
