49997d793dac759b6fe56d7bfed14bbc789e4673
Joshua Byrd <phocks@gmail.com>
2021-08-23 11:18:23 +1000

    Bake bash script

---
+++ run_import.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Load nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 14.17.5
+
+# now node works
+node -e "console.log('Node version:')"
+node --version
+
+# npm works too!
+node -e "console.log('npm version:')"
+npm --version
+
+node src/index.js
