b8880d9cb5126e8b8d04866d74f31775873aa426
Katia Shatoba <katya.shatoba@gmail.com>
2021-12-06 14:00:27 +1100

    do not include 07/01 in aus doses

--- src/vaccinations/index.js
+++ src/vaccinations/index.js
@@ -400,9 +400,14 @@ function addDays(date, days) {
 function parseDosesBreakdownData(data, locationTotals) {
   const array = [];
   data.forEach((entry) => {
+
     // we use date reported instead of date as at, so add one day to the set as date
     let date = format(addDays(new Date(entry["DATE_AS_AT"]), 1), "yyyy/MM/dd");
 
+    if (addDays(new Date(entry["DATE_AS_AT"]), 1) < new Date("2021/07/02")) {
+      return;
+    }
+    
     const locations = ['ACT', 'NT', 'VIC', 'NSW', 'SA', 'TAS', 'QLD', 'WA'];
 
     let ausPopulation12to15 = 0;
