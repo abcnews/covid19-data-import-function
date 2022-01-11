70f0c49c70039775156c08eb921311154a448a3d
Joshua Byrd <phocks@gmail.com>
2022-01-11 13:05:37 +1000

    Cleaning up a bit

--- src/index.js
+++ src/index.js
@@ -563,42 +563,6 @@ const main = async () => {
 
     // User feedback
     console.log("Uploaded to FTP...", ftpResponse);
-
-    //     console.log(`Johns Hopkins data:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/data.json
-    // Also:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/country-totals.json
-    // And:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/after-100-cases.json
-
-    // WHO country totals:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/who-country-totals.json
-    // WHO after 100:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/who-after-100-cases.json
-
-    // ECDC country totals:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/ecdc-country-totals.json
-    // ECDC after 100:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/ecdc-after-100-cases.json
-
-    // ABC hybrid country totals:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/hybrid-country-totals.json
-    // ABC hybrid after 100:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/hybrid-after-100-cases.json
-
-    // ABC hybrid extra data (deaths etc.):
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/country-totals-extra.json
-
-    // Combined data with deaths and recovered and regions etc:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/places-totals.json
-
-    // DSI source of infection data:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/dsi-local-acquisition.json
-
-    // Individual places also available at eg:
-    // https://www.abc.net.au/dat/news/interactives/covid19-data/places/australia.json
-    // Just change the filename to the place you want (slugified) eg. new-zealand.json`);
-
     console.log(
       "Data lookup: https://www.abc.net.au/dat/news/interactives/covid19-data/places-lookup.json"
     );
