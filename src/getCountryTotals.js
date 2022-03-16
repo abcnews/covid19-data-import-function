e4ec35b605fbbbd79187280842c314c6693627af
Joshua Byrd <phocks@gmail.com>
2022-03-16 16:22:10 +1000

    Do not include some totals

--- src/getCountryTotals.js
+++ src/getCountryTotals.js
@@ -1,5 +1,5 @@
 // Transform data adding together country totals
-const getCountryTotals = countries => {
+const getCountryTotals = (countries) => {
   const newCountries = {};
 
   for (const country of countries) {
@@ -7,6 +7,12 @@ const getCountryTotals = countries => {
   }
 
   for (const country of countries) {
+    // For now we are skipping Hong Kong
+    if (country["Province/State"] === "Hong Kong") {
+      console.log("NOTE: Hong Kong is a special case. DO NOT INCLUDE IN CHINA TOTALS FOR NOW.");
+      continue;
+    }
+
     for (const total of country.Cases) {
       if (
         typeof newCountries[country["Country/Region"]][total.Date] ===
